8996f8014c3efc023c4c96570283b8dd5ffc3b16
bmarquis <dev@bmarquis.eu>
2026-01-28 17:57:44 +0100

    feat: values

---
+++ ABM.Paie.Formation/Resources/Values.cs
@@ -0,0 +1,25 @@
+﻿using Common.Models;
+using System.Collections.Immutable;
+
+namespace ABM.Paie.Formation.Resources;
+
+public static class Values
+{
+    public enum Key
+    {
+        Respect,
+        Quality,
+        Organization,
+        Originality,
+        Curiosity,
+    }
+
+    public static readonly ImmutableDictionary<Key, BarModel> Dictionary = ImmutableDictionary.CreateRange(new KeyValuePair<Key, BarModel>[]
+    {
+        new(Key.Respect, new("Respect", 3)),
+        new(Key.Quality, new("Qualité", 1)),
+        new(Key.Organization, new("Animation", 4)),
+        new(Key.Originality, new("Originalité", 2)),
+        new(Key.Curiosity, new("Curiosité", 5)),
+    });
+}
---
+++ Common/Models/BarModel.cs
@@ -0,0 +1,3 @@
+﻿namespace Common.Models;
+
+public record BarModel(string Word, int Position);
